349fbfb29c8cf71068915ecf529e8724e5954603
kyky4 <kyky43678@gmail.com>
2023-01-25 02:07:18 +0200

    first commit homework 3

---
+++ Homework/Homework3.py
@@ -0,0 +1,16 @@
+num = int(input('Введите 5-ти значное число: '))
+inference = num % 10
+after = num // 10
+last_num = after % 10
+after = after // 10
+inference = inference * 10 + last_num
+last_num = after % 10
+after = after // 10
+inference = inference * 10 + last_num
+last_num = after % 10
+after = after // 10
+inference = inference * 10 + last_num
+last_num = after % 10
+after = after // 10
+inference = inference * 10 + last_num
+print(inference)
