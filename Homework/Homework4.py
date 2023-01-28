939261289e914b8c8c7c24bc781db62e4379c400
kyky4 <kyky43678@gmail.com>
2023-01-28 22:17:30 +0200

    first commit

---
+++ Homework/Homework4.py
@@ -0,0 +1,16 @@
+num_one = int(input('Введите первое число: \n'))
+num_two = int(input('Введите второе число: \n'))
+operation = int(input('Выберите номер операции , где: \n1 это (+) \n2 это (-) \n3 это (*) \n4 это (/) \n'))
+
+if operation == 1:
+    print('Сумма чисел равна', num_one + num_two)
+elif operation == 2:
+    print('Разность чисел равна', num_one - num_two)
+elif operation == 3:
+    print('Произведение чисел равно', num_one * num_two)
+elif operation == 4 and num_two == 0:
+    print('Операция невозможна , на ноль делить нельзя')
+elif operation == 4:
+    print('Частное чисел равно', num_one / num_two)
+else:
+    print('Неизвестная операция')
