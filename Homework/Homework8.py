34438921feed2cb96c1b9c0df238e6f360e460b3
kyky4 <kyky43678@gmail.com>
2023-02-05 21:54:49 +0200

    commit

--- Homework/Homework8.py
+++ Homework/Homework8.py
@@ -2,12 +2,9 @@ lst = [1, 3, 5]
 #lst = [6]
 #lst = []
 sum = 0
-i = 0
 if len(lst):
-    while i < len(lst):
-        x = lst[i]
-        sum += x
-        i += 2
+    for i in range(0, len(lst), 2):
+        sum += lst[i]
     print(sum * lst[-1])
 else:
     print(0)
